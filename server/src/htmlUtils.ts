d59d1a9c0fedb87ba7724170f473fb244a8e9c36
Dan Gabel <dan.gabel@gmail.com>
2026-05-30 09:39:51 -0500

    cdns

--- server/src/htmlUtils.ts
+++ server/src/htmlUtils.ts
@@ -52,6 +52,8 @@ export function generateErrorHtml(
 function generateBaseHtmlTemplate(title: string, chartContent: string): string {
   return `
       <script src="https://trmnl.com/js/highcharts/12.3.0/highcharts.js"></script>
+      <script src="https://trmnl.com/js/highcharts/12.3.0/highcharts-more.js"></script>
+      <script src="https://trmnl.com/js/highcharts/12.3.0/pattern-fill.js"></script>
       <script src="https://trmnl.com/js/chartkick/5.0.1/chartkick.min.js"></script>
 
         <div class="layout">
